788f9dd24ce7efbbbfa8f164e7dcee3e8e304fe5
Tensorboy2 <svargdal@online.no>
2025-05-16 13:00:21 +0200

    trying large

--- project_2/models/convnext.py
+++ project_2/models/convnext.py
@@ -146,7 +146,7 @@ def ConvNeXtLarge(pre_trained = False):
 if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(device)
-    x = torch.randn((3,1,128,128)).to(device)
-    model = ConvNeXtXL().to(device)
+    x = torch.randn((3,1,128,128)).half().to(device)
+    model = ConvNeXtLarge().half().to(device)
     print(model)
     print(model(x).cpu().detach().numpy())
