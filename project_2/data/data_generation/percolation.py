38c4fe117579f8aedfe7535dbc72f85553b055aa
Tensorboy2 <svargdal@online.no>
2025-06-03 14:45:53 +0200

    prediction documentation

--- project_2/data/data_generation/percolation.py
+++ project_2/data/data_generation/percolation.py
@@ -8,9 +8,11 @@ import os
 STRUCTURE_8 = np.ones((3, 3), dtype=int)
 SCRIPT_DIR = os.path.dirname(__file__)
 
-# === IMAGE GENERATION ===
 
 def generate_periodic_blobs(shape=(128, 128), density=0.5, sigma=2.0, seed=0):
+    '''
+    Modified from the scipy.ndimage.binary_blobs function to include mode "wrap", for periodic wrapping on structures.
+    '''
     rng = np.random.default_rng(seed)
     noise = rng.random(shape)
     smooth = gaussian_filter(noise, sigma=sigma, mode='wrap')
@@ -35,7 +37,6 @@ def generate_cross_channels(shape=(64, 64), width=3):
 
     return img
 
-# === PERCOLATION LOGIC ===
 
 def label_fluid_regions(binary_img):
     """Labels fluid regions where img == False."""
@@ -47,6 +48,7 @@ def detect_percolation(binary_img):
     labeled, _ = label_fluid_regions(dilated)
     h, w = labeled.shape
 
+    # Boundary check:
     x_perc = any(labeled[0, j] != 0 and labeled[0, j] == labeled[-1, j] for j in range(w))
     y_perc = any(labeled[i, 0] != 0 and labeled[i, 0] == labeled[i, -1] for i in range(h))
 
@@ -65,9 +67,11 @@ def fill_non_percolating_fluid(binary_img):
     filled_img[non_perc_mask] = True  # fill in fluid to solid
     return filled_img
 
-# === PLOTTING ===
 
 def plot_percolation_results(original, labeled, filled, percolates_x, percolates_y, save_path=None):
+    '''
+    Plotting of the original image, the labeled fluid clusters and the filled image.
+    '''
     num_labels = labeled.max()
     jet_colors = plt.cm.jet(np.linspace(0, 1, num_labels))
     colors = np.vstack(([1, 1, 1, 1], jet_colors))
@@ -91,7 +95,6 @@ def plot_percolation_results(original, labeled, filled, percolates_x, percolates
     else:
         plt.show()
 
-# === MAIN EXAMPLE ===
 
 def main():
     # Example: switch between blob or cross image here
